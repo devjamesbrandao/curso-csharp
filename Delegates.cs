3e8865927c0a7b47dbea7d967199932a486ad708
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-02-21 11:40:20 -0300

    Estudos sobre delegate

---
+++ Delegates.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Abrir_e_ecrever_em_arquivo
+{
+
+    class Retangulo
+    {
+
+        // Declarando delegate
+        public delegate void retanguloDelegate(double altura, double largura);
+
+
+        // Declarando método "area"
+        public void area(double altura, double largura)
+        {
+            Console.WriteLine("Área é: {0}", (largura * altura));
+        }
+
+
+        // Declarando método "perimetro"
+        public void perimetro(double altura, double largura)
+        {
+            Console.WriteLine("Perímetro é: {0} ", 2 * (largura + altura));
+        }
+
+
+        // Método Main: entryPoint da aplicação
+        public static void Main(String[] args)
+        {
+
+            // Criando o objeto da classe
+            // "Retangulo" é nomeado como "ret"
+            Retangulo ret = new Retangulo();
+
+            // Criando objeto delegate nomeado como "retDele"
+            // e passando o método "area" como parâmetro por meio do objeto da classe "ret"
+            retanguloDelegate retDele = new retanguloDelegate(ret.area);
+
+            // Multicasting do delegate: meio que une a chamada dos dois métodos
+            retDele += ret.perimetro;
+
+            Console.Title = "Uso de delegates";
+
+            // Passando os valores como parâmetro para os dois métodos (area e perimetro)
+            // utilizando o operador "Invoke" nativo dos delegates
+            retDele.Invoke(6.3, 4.2);
+            Console.WriteLine();
+
+            // Chamando os métodos novamente com outros valores
+            retDele.Invoke(16.3, 10.3);
+            Console.WriteLine();
+
+            // Removendo a atribuição do método "perimetro"
+            retDele -= ret.perimetro;
+
+            retDele.Invoke(2, 5);
+        }
+    }
+}
